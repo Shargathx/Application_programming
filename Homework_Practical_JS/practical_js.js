75f5b6e59d0788b40901f545c2a1db1ccbcc4dce
Shargathx <martin.saarevali@hotmail.com>
2026-09-12 14:37:09 +0300

    docs: reorganize files

---
+++ Homework_Practical_JS/practical_js.js
@@ -0,0 +1,9 @@
+const users = [
+    { id: 1, name: "Mari", age: 22, active: true },
+    { id: 2, name: "Jüri", age: 17, active: false },
+    { id: 3, name: "Kati", age: 31, active: true },
+    { id: 4, name: "Martin", age: 19, active: false },
+    { id: 5, name: "Laura", age: 26, active: true }
+]
+
+console.log(users);
